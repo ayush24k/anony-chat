af803d5ea8422f591d26026c1611dfd045054499
Ayush <ayushanshu69@gmail.com>
2024-03-09 18:12:40 +0530

    error fix

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ const app = express();
 const server = createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: 'https://anony-chat-front.vercel.app/',
+        origin: 'https://anony-chat-front.vercel.appa',
         methods: ["GET", "POST"],
     },
 })

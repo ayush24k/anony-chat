9a800c1ec7743065bc99f7a8bf5adb0ce03b76b8
Ayush <ayushanshu69@gmail.com>
2024-03-09 17:43:45 +0530

    error fix i guess

--- client/app/components/ChatPage/ChatPage.tsx
+++ client/app/components/ChatPage/ChatPage.tsx
@@ -9,7 +9,7 @@ const socket = io('https://anony-chat-backend.vercel.app/')
 
 export default function ChatPage() {
 
-    const [messages, setMessage] = useState([]);
+    const [messages, setMessage]:any = useState([]);
     const [messageInput, setMessageInput] =useState('');
 
     useEffect(() => {

daa727a765e20636426f428d6ba35e9763928459
Ayush <ayushanshu69@gmail.com>
2024-03-30 22:32:43 +0530

    new page

--- client/app/whyUseOurWebsite/page.tsx
+++ client/app/whyUseOurWebsite/page.tsx
@@ -1,4 +1,5 @@
 import "./why.css"
+import Link from "next/link"
 
 export default function page() {
   return (
@@ -9,9 +10,19 @@ export default function page() {
           <h4>here's why</h4>
           <li>
             <ul>
-              - he fsh js c dhjads daacjhsdc
+              - its fun i guess.
+            </ul>
+            <ul>
+              - it will make me happy :).
+            </ul>
+            <ul>
+              - your chat is completely safe and gets deleted after every session.(i didnt put efforts to set up an database to store messages)
             </ul>
           </li>
+        </div>
+        <img src="/Image.png" />
+        <div className="whyLink">
+          <Link href="/" className="link">return to chat page</Link>
         </div>
       </div>
     </>
